827aa19d631d6f55b3013406807f5942ef3536e0
lexoice <lexoice@gmail.com>
2025-06-11 13:13:33 +0500

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,12 @@ function MyApp({ Component, pageProps, makes }) {
 
       <Script
         src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`}
-        strategy="afterInteractive"
+        strategy="lazyOnload"
+        onLoad={() => {
+          setTimeout(() => {
+
+          }, 2000)
+        }}
       />
 
       <ModalProvider>
--- pages/_document.js
+++ pages/_document.js
@@ -8,24 +8,26 @@ export default function Document() {
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              window.$zopim || (function(d, s) {
-                var z = $zopim = function(c) {
-                  z._.push(c)
-                },
-                $ = z.s = d.createElement(s),
-                e = d.getElementsByTagName(s)[0];
-                z.set = function(o) {
-                  z.set._.push(o)
-                };
-                z._ = [];
-                z.set._ = [];
-                $.async = !0;
-                $.setAttribute('charset', 'utf-8');
-                $.src = '//v2.zopim.com/?3qAPIc9GuejgNw39FMermZNLtKuAiBJg';
-                z.t = +new Date;
-                $.type = 'text/javascript';
-                e.parentNode.insertBefore($, e)
-              })(document, 'script');
+              setTimeout(function() {
+                window.$zopim || (function(d, s) {
+                  var z = $zopim = function(c) {
+                    z._.push(c)
+                  },
+                  $ = z.s = d.createElement(s),
+                  e = d.getElementsByTagName(s)[0];
+                  z.set = function(o) {
+                    z.set._.push(o)
+                  };
+                  z._ = [];
+                  z.set._ = [];
+                  $.async = !0;
+                  $.setAttribute('charset', 'utf-8');
+                  $.src = '//v2.zopim.com/?3qAPIc9GuejgNw39FMermZNLtKuAiBJg';
+                  z.t = +new Date;
+                  $.type = 'text/javascript';
+                  e.parentNode.insertBefore($, e)
+                })(document, 'script');
+              }, 2000);
             `,
           }}
         />
