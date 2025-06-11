20045260af768b26c422df939be20e143d88959d
lexoice <lexoice@gmail.com>
2025-06-11 13:55:06 +0500

    zendesk fix

--- pages/_document.js
+++ pages/_document.js
@@ -8,26 +8,28 @@ export default function Document() {
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              setTimeout(function() {
-                window.$zopim || (function(d, s) {
-                  var z = $zopim = function(c) {
-                    z._.push(c)
-                  },
-                  $ = z.s = d.createElement(s),
-                  e = d.getElementsByTagName(s)[0];
-                  z.set = function(o) {
-                    z.set._.push(o)
-                  };
-                  z._ = [];
-                  z.set._ = [];
-                  $.async = !0;
-                  $.setAttribute('charset', 'utf-8');
-                  $.src = '//v2.zopim.com/?3qAPIc9GuejgNw39FMermZNLtKuAiBJg';
-                  z.t = +new Date;
-                  $.type = 'text/javascript';
-                  e.parentNode.insertBefore($, e)
-                })(document, 'script');
-              }, 2000);
+              document.addEventListener('DOMContentLoaded', function() {
+                setTimeout(function() {
+                  window.$zopim || (function(d, s) {
+                    var z = $zopim = function(c) {
+                      z._.push(c)
+                    },
+                    $ = z.s = d.createElement(s),
+                    e = d.getElementsByTagName(s)[0];
+                    z.set = function(o) {
+                      z.set._.push(o)
+                    };
+                    z._ = [];
+                    z.set._ = [];
+                    $.async = !0;
+                    $.setAttribute('charset', 'utf-8');
+                    $.src = '//v2.zopim.com/?3qAPIc9GuejgNw39FMermZNLtKuAiBJg';
+                    z.t = +new Date;
+                    $.type = 'text/javascript';
+                    e.parentNode.insertBefore($, e)
+                  })(document, 'script');
+                }, 5000);
+              });
             `,
           }}
         />
